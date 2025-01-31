7fb9a2a49c299f02fa851f295982cdd125b8ad5b
Irwing <Irwingvazquez@hotmail.com>
2025-01-31 01:12:21 -0600

    first commit

---
+++ BankAccount.java
@@ -0,0 +1,23 @@
+public class BankAccount {
+    public static void main(String[] args) {
+        
+        float BobAccount = 2175.0F;
+
+        BobAccount -= 302.50F;  
+
+        BobAccount +=  50.03F;
+
+        BobAccount /= 2;
+
+        BobAccount += 20.00F;
+
+        BobAccount -= 1;
+
+        BobAccount *= 2;
+
+        BobAccount += 1;
+
+        System.out.println(BobAccount);
+
+    }
+}
